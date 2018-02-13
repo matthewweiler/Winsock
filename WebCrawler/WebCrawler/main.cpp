580aa57c52a1b4d076ec726fc167fa4168109537
Matthew Weiler <matthewweiler96@gmail.com>
2018-02-12 21:01:27 -0500

    Some weird errors

--- WebCrawler/WebCrawler/main.cpp
+++ WebCrawler/WebCrawler/main.cpp
@@ -13,7 +13,7 @@ unordered_set<string> HostsUnique;
 unordered_set<string> IPUnique;
 ifstream fin;
 ofstream fout;
-enum Request {head, getr, robot};
+enum Request { head, getr, robot };
 mutex mutexPrint;
 class Param {
 public:
@@ -37,30 +37,22 @@ bool UniqueHost(string host) {
 	}
 }
 
-bool UniqueIP(string host, short port){
-	struct hostent *remote;
-	if ((remote = gethostbyname(host.c_str())))
-	{
-		printSafe("Invalid host name string: not FQDN\n");
-		return false;  // 1 means failed
+bool UniqueIP(string host) {
+	std::unordered_set<std::string>::const_iterator it = IPUnique.find(host);
+	if (it == IPUnique.end()) {
+		return true;
 	}
 	else {
-		std::unordered_set<std::string>::const_iterator it = IPUnique.find(host);
-		if (it == IPUnique.end()) {
-			return true;
-		}
-		else {
-			return false;
-		}
+		return false;
 	}
 }
 
 string constructRequest(Request r, URLParser* p) {
 	switch (r) {
-		case robot: return "HEAD /robots.txt HTTP/1.0\nHost: " + p->getHost() + "\n\n";
-		case head: return "HEAD " + p->getPath() + p->getQuery() + " HTTP/1.0\nHost: " + p->getHost() + "\n\n";
-		case getr: return "GET " + p->getPath() + p->getQuery() + " HTTP/1.0\nHost: " + p->getHost() + "\n\n";
-		default: return "HEAD /robots.txt HTTP/1.0\nHost: " + p->getHost() + "\n\n";
+	case robot: return "HEAD /robots.txt HTTP/1.0\nHost: " + p->getHost() + "\n\n";
+	case head: return "HEAD " + p->getPath() + p->getQuery() + " HTTP/1.0\nHost: " + p->getHost() + "\n\n";
+	case getr: return "GET " + p->getPath() + p->getQuery() + " HTTP/1.0\nHost: " + p->getHost() + "\n\n";
+	default: return "HEAD /robots.txt HTTP/1.0\nHost: " + p->getHost() + "\n\n";
 	}
 }
 
@@ -73,7 +65,7 @@ string getURL() {
 	return "";
 }
 
-void ConnectandSend(URLParser parser) {
+void ConnectandSend(URLParser parser, string& message) {
 	Winsock::initialize();	// initialize 
 
 	Winsock ws;
@@ -89,14 +81,29 @@ void ConnectandSend(URLParser parser) {
 	// test for robot file
 	string req = constructRequest(robot, &parser);
 	ws.sendRequest(req);
+	message += "sent request\n";
 
 	// receive robot reply
 	string response = "";
 	if (ws.receive(response) == 0) {
 		printSafe(response + "\n");
 	}
+	message += "response received\n";
 
 	//if no robot, send get request
+	if (response[9] != '2') {
+		string get_req = constructRequest(getr, &parser);
+		ws.closeSocket();
+		ws.cleanUp();
+		Winsock ws;
+		ws.createTCPSocket();
+		ws.connectToServer(host, port);
+		ws.sendRequest(get_req);
+		response = "";
+		if (ws.receive(response) == 0)
+			message += "response received\n";
+		//printSafe(response + "\n");
+	}
 	//else, do not send request
 
 	ws.closeSocket();
@@ -110,37 +117,71 @@ UINT threadFunction(LPVOID pParam) {
 	Param *p = (Param *)pParam;
 	HANDLE arr[] = { p->eventQ, p->mutex };
 	string url = "";
-	while (true){
+	string message = "";
+	while (true) {
 		if (WaitForMultipleObjects(2, arr, false, INFINITE) == WAIT_OBJECT_0) {
 			break;
 		}
-		else { //Thread obtains mutex
+		else { //one Thread obtains mutex
+			printSafe(message);
+			message = "";
 			url = getURL();
-			cout << url << endl;
+			string message = url + "\n";
+			//cout << url << endl;
 			URLParser parser(url);
 			bool uh;
 			bool ui;
-			if (url.compare("")) {
+			if (!url.compare("")) {
 				uh = UniqueHost(parser.getHost());
-				ui = UniqueIP(parser.getHost(), parser.getPort());
+				if (uh) {
+					message += "unique host\n";
+					HostsUnique.insert(parser.getHost());
+				}
+				else {
+					message += "NOT a unique host\n";
+					continue;
+				}
+
+				// get IP
+				struct sockaddr_in server; // structure for connecting to server
+				struct hostent *remote;    // structure used in DNS lookups: convert host name into IP address
+
+				if ((remote = gethostbyname(parser.getHost().c_str())) == NULL) {
+					message += "failed to get IP\n";
+					//printf("Invalid host name string: not FQDN\n");
+					continue;  // 1 means failed
+				}
+				else // take the first IP address and copy into sin_addr 
+				{
+					memcpy((char *)&(server.sin_addr), remote->h_addr, remote->h_length);
+				}
+
+				ui = UniqueIP((char*)&server.sin_addr);
+				//ui = UniqueIP(parser.getHost(), parser.getPort());
+				if (ui) {
+					message += "unique IP\n";
+					IPUnique.insert((char*)&server.sin_addr);
+				}
+				else {
+					message += "NOT unique IP\n";
+					continue;
+				}
+				/*
 				if (uh && ui) {
 					//Add the unique hosts here.
 					HostsUnique.insert(parser.getHost());
 					//Change to add IPs not hosts
 					IPUnique.insert(parser.getHost());
 				}
+				*/
+
 			}
 			else {
 				SetEvent(p->eventQ);
 			}
 			ReleaseMutex(p->mutex);
-			if (uh && ui) {
-				ConnectandSend(parser);
-				//free(&parser);
-			}
-			else {
-				//Do nothing for now. Output if IP is not Unique.
-			}
+			ConnectandSend(parser, message);
+			//printSafe(message);
 		}
 	}
 	return 0;
@@ -152,7 +193,7 @@ int main(int argc, char **argv)
 	if (argc > 1)
 	{
 		numThreads = atoi(argv[1]);
-		filename = argv[2]; 
+		filename = argv[2];
 		printf("number of threads is %d, filename is %s\n\n",
 			numThreads, filename.c_str());
 	}
@@ -165,7 +206,7 @@ int main(int argc, char **argv)
 	// fout.open("crawldata.txt");
 	if (fin.fail()) // cannot open this file
 	{
-		printSafe("NO such a file. Failed to open it\n"); 
+		printSafe("NO such a file. Failed to open it\n");
 		getchar();
 		return 0;
 	}
